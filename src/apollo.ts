5b6470742b3f8804165213803b6b9be613a0f6fe
ROHHYEONMIN <roh._.hm@RohHyeonMinui-MacBookPro.local>
2022-09-13 18:28:32 +0900

    connect api server

--- src/apollo.ts
+++ src/apollo.ts
@@ -7,7 +7,10 @@ export const isLoggedInVar = makeVar(Boolean(token));
 export const authTokenVar = makeVar(token);
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:4000/graphql',
+    uri:
+        process.env.NODE_ENV === 'production' 
+            ? "https://mmm-backend.herokuapp.com/graphql"
+            : 'http://localhost:4000/graphql',
 });
 
 const authLink = setContext((_, { headers }) => {
